628e1ff31f81da1e9f86467eb548b22d0f3b32ef
unknown <islammohamedahmed49@gmail.com>
2024-04-12 22:14:44 +0200

    another one bites the dust

---
+++ AnotherOneBitesTheDust/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+long a, b, c;
+
+string str = ReadLine();
+
+var arr = str.Split(" ");
+
+a = ToInt64(arr[0]);
+b = ToInt64(arr[1]);
+c = ToInt64(arr[2]);
+
+if(a == b)
+{
+    WriteLine(a + b + c * 2);
+}
+else
+{
+    WriteLine(Min(a,b)*2 + c * 2 + 1);
+}
